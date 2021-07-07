9c37a1886aef20cfc77cbcb8d9822bb0b35ee1ae
Janet Swisher <janet.swisher@openfin.co>
2021-07-07 16:08:32 -0500

    Fix run-on sentence
    Also the deleted "it" was ambiguous.

--- how-to/add-an-application-to-workspace/desktop-owner-settings.bat
+++ how-to/add-an-application-to-workspace/desktop-owner-settings.bat
@@ -3,9 +3,9 @@ reg query HKEY_CURRENT_USER\Software\OpenFin\RVM\Settings -v DesktopOwnerSetting
 
 if %ERRORLEVEL% EQU 0 (
 	echo Warning: You already have a desktop owner settings value as shown below. 
-	echo If you choose to overwrite it will set this value, you will need to restore it later.
+	echo If you choose to overwrite this value, you will need to restore it later.
 	echo If you are not sure what this means, please choose No at the prompt below and 
-	echo Review https://developers.openfin.co/of-docs/docs/desktop-owner-settings
+	echo refer to https://developers.openfin.co/of-docs/docs/desktop-owner-settings
 	echo.
 	reg query HKEY_CURRENT_USER\Software\OpenFin\RVM\Settings -v DesktopOwnerSettings
 
